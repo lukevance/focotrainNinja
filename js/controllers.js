cd109cf553991787cf307fd8542181c32062563f
Brandon H Bodine <brandonhbodine@gmail.com>
2016-07-08 19:51:47 -0600

    Added a change the input function

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,23 @@
+'use strict';
 angular.module('templateApp')
   .controller('MainController', [MainController]);
 
 function MainController () {
   var vm = this;
-
+  vm.curIndex = 0;
   vm.questions = [
     "Are you sure?",
     "Ya like rum mon?",
     "What's your zip?",
     "D'ya wanna leave today mon?",
-    "Wanna take a train mon?",
-
+    "Wanna take a train mon?"
   ];
 
-  
+  vm.cur = vm.questions[0];
+  vm.change = function(){
+    console.log("clicked");
+    vm.curIndex++;
+    vm.cur = vm.questions[vm.curIndex];
+  };
+
 }
